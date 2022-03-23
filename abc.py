b9dd79bf1d1552da39c9ca75929e46462a4b3759
AnushreeBVPathar <67868969+AnushreeBVPathar@users.noreply.github.com>
2022-03-23 15:03:00 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,66 +1,66 @@
-def aStartAlgo(start_node,stop_node):
-    open_set=set(start_node)
-    closed_set=set()
+def astar(startnode,stopnode):
+    os=set(startnode)
+    cs=set()
     g={}
     parents={}
-    g[start_node]=0
-    parents[start_node]=start_node
-    while len(open_set)>0:
+    g[startnode]=0
+    parents[startnode]=startnode
+    while len(os)>0:
         n=None
-        for v in open_set:
-            if n==None or g[v] + heuristic(v)<g[n]+heuristic(n):
+        for v in os:
+            if n==None or g[v]+heu[v]<g[n]+heu[n]:
                 n=v
-        if n==stop_node or Graph_nodes[n]==None:
+        if n==stopnode or Graphnode[n]==None:
             pass
         else:
-            for(m,weight) in get_neighbors(n):
-                if m not in open_set and m not in closed_set:
-                    open_set.add(m)
+            for (m,wei) in get_nei(n):
+                if m not in os and m not in cs:
+                    os.add(m)
                     parents[m]=n
-                    g[m]=g[n]+weight
+                    g[m]=g[n]+wei
                 else:
-                    if g[m]>g[n]+weight:
-                        g[m]=g[n]+weight
+                    if g[m]>g[n]+wei:
+                        g[m]=g[n]+wei
                         parents[m]=n
-                        if m in closed_set:
-                            closed_set.remove(m)
-                            open_set.add(m)
+                        if m in cs:
+                            cs.remove(m)
+                            os.add(m)
         if n==None:
-            print('Path does not exist!')
+            print("path not found")
             return None
-        if n==stop_node:
+        if n==stopnode:
             path=[]
             while parents[n]!=n:
                 path.append(n)
                 n=parents[n]
-            path.append(start_node)
+            path.append(startnode)
             path.reverse()
-            print('Path found:{}'.format(path))
+            print("path found:{}".format(path))
             return path
-        open_set.remove(n)
-        closed_set.add(n)
-    print('Path does not exist!')
+        os.remove(m)
+        cs.add(m)
+    print("Path not exists")
     return None
-def get_neighbors(v):
-    if v in Graph_nodes:
-        return Graph_nodes[v]
+def get_nei(v):
+    if v in Graphnode[v]:
+        return Graphnode[v]
     else:
         return None
-def heuristic(n):
-    H_dist={
+def heu(n):
+    h_dist={
         'A':11,
         'B':6,
         'C':99,
         'D':1,
-        'E':7,
-        'G':0,
+        'E':0,
+        'G':7
     }
-    return H_dist[n]
-Graph_nodes={
-    'A':[('B',2),('E',3)],
+    return h_dist[n]
+Graphnode={
+    'A':[('B',2),('C',3)],
     'B':[('C',1),('G',9)],
     'C':None,
     'E':[('D',6)],
-    'D':[('G',1)],
+    'D':[('G',1)]
 }
-aStartAlgo('A','G')
+astar('A','G')
